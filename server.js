732e689f54cd9bc5544f8b68564cc19ad1bac170
Selman Baskaya <selmanbaskaya1@gmail.com>
2022-01-25 12:41:22 +0300

    sripts and constant modules created, new event listener added to socketio

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const socket = require("socket.io");
 
+const fs = require("fs");
+const axios = require("axios");
+const iot = require("./src/scripts/IoTStreamer");
+const constant = require("./src/constant/Constant");
+
 const app = express();
 const server = app.listen(process.env.PORT || 5100);
 
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const io = socket(server, {
   cors: {
@@ -24,5 +29,29 @@ io.on("connection", (socket) => {
 
     const response = { message: `User (${data.name}) has been added!` };
     io.sockets.emit("userInfoResponse", response);
+  });
+
+  socket.on("iotData", (data) => {
+    axios
+      .post(data.url, data.body)
+      .then((res) => {
+        /* const iotValues = iot.extractResponseValueByKey(res.data, data.valuePath);
+        const newLine = "\r\n" + iotValues[data["xLabel"]][0] + ", " + iotValues[data["yLabel"]][0];
+        const csvFilePath = constant.PYTHON_TASK_DIR + data["userId"] + "/twinapi/" + data["fileName"]
+
+        if (!fs.existsSync(csvFilePath)) {
+          fs.writeFileSync(csvFilePath, "time,value");
+        }
+        fs.appendFileSync(csvFilePath, newLine); */
+
+        if (false) {
+          io.sockets.emit("response", { warningMsg: "IoT streaming has been reached its the limit value!" });
+        } else {
+          io.sockets.emit("response", { responseData: res.data });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   });
 });
---
+++ src/constant/Constant.js
@@ -0,0 +1,5 @@
+const PYTHON_TASK_DIR = "/Users/simularge/Desktop/public/"
+
+module.exports = {
+    PYTHON_TASK_DIR: PYTHON_TASK_DIR
+}
