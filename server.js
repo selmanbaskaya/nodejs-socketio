bc8f26ee9167e4c16e41ae88e4f9ea265b1e971d
Selman Baskaya <selmanbaskaya1@gmail.com>
2022-01-25 12:45:02 +0300

    unused code lines commented

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const socket = require("socket.io");
 
-const fs = require("fs");
+/* const fs = require("fs");
 const axios = require("axios");
 const iot = require("./src/scripts/IoTStreamer");
-const constant = require("./src/constant/Constant");
+const constant = require("./src/constant/Constant"); */
 
 const app = express();
 const server = app.listen(process.env.PORT || 5100);
