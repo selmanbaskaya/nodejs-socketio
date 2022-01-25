5e541133da2f330542f85f5cf30de01d6d3d4eae
Selman Baskaya <selmanbaskaya1@gmail.com>
2022-01-25 15:25:00 +0300

    extractResponseValueByKey func has been implemented

--- server.js
+++ server.js
@@ -31,11 +31,19 @@ io.on("connection", (socket) => {
     io.sockets.emit("userInfoResponse", response);
   });
 
-  socket.on("iotData", (data) => {
-    axios
+  socket.on("iotData", async (data) => {
+    await axios
     .post(data.url, data.body)
     .then((res) => {
         console.log('RESPONSE: ', res.data);
+        const iotValues = iot.extractResponseValueByKey(res.data, data.valuePath);
+        const newLine = "\r\n" + iotValues[data["xLabel"]][0] + ", " + iotValues[data["yLabel"]][0];
+        console.log('New Csv File Line: ', newLine);
+        /* const csvFilePath = constant.PYTHON_TASK_DIR + data["userId"] + "/twinapi/" + data["fileName"]
+        if (!fs.existsSync(csvFilePath)) {
+          fs.writeFileSync(csvFilePath, "time,value");
+        }
+        fs.appendFileSync(csvFilePath, newLine); */
     })
     .catch((error) => {
       console.error(error);
