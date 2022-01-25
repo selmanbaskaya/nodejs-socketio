ac87e11b2f678b07ca182e28abf5ac42f215a395
Selman Baskaya <selmanbaskaya1@gmail.com>
2022-01-25 12:56:11 +0300

    last 3 commits reverted for deploy

--- src/constant/Constant.js
+++
@@ -1,5 +0,0 @@
-const PYTHON_TASK_DIR = "/Users/simularge/Desktop/public/"
-
-module.exports = {
-    PYTHON_TASK_DIR: PYTHON_TASK_DIR
-}
--- src/scripts/IoTStreamer.js
+++
@@ -1,7 +0,0 @@
-function extractResponseValueByKey(response, path) {
-    return path.split('.').reduce((previous, current) => previous[current], response)
-}
-
-module.exports = {
-    extractResponseValueByKey: extractResponseValueByKey
-}
